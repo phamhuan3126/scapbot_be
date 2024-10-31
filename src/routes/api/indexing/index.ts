3e6466d827a0fdb1e01984082d4192dd05dadceb
phamhuan3126 <phamhuan.net@gmail.com>
2024-10-31 17:02:13 +0700

    indexing

--- src/routes/api/indexing/index.ts
+++ src/routes/api/indexing/index.ts
@@ -1,4 +1,5 @@
 import { FastifyPluginAsync } from "fastify";
+import validator from "validator";
 
 const indexing: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   const handleError = (error: any, request: any, reply: any) => {
@@ -63,7 +64,43 @@ const indexing: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
             .status(404)
             .send({ error: "Index request not found or access denied" });
         }
-        if (
+
+        //Check point need
+        if (status === "RUNNING" && indexRequest.status === "DRAFT") {
+          const totalCredit = await fastify.prisma.transaction.aggregate({
+            _sum: {
+              points: true,
+            },
+            where: {
+              userId,
+              type: "CREDIT",
+            },
+          });
+
+          const totalDebit = await fastify.prisma.transaction.aggregate({
+            _sum: {
+              points: true,
+            },
+            where: {
+              userId,
+              type: "DEBIT",
+            },
+          });
+
+          const totalPoints =
+            (totalCredit._sum.points || 0) - (totalDebit._sum.points || 0);
+          const pointsNeeded = await fastify.prisma.indexLink.count({
+            where: { requestId: parseInt(id, 10) },
+          });
+
+          if (totalPoints < pointsNeeded) {
+            return reply
+              .status(400)
+              .send({
+                error: "Not enough points to update status to RUNNING.",
+              });
+          }
+        } else if (
           status &&
           !(indexRequest.status === "DRAFT" && status === "RUNNING")
         ) {
@@ -71,6 +108,8 @@ const indexing: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
             error: "Status can only be updated from DRAFT to RUNNING",
           });
         }
+
+        //Update database
         const updatedIndexRequest = await fastify.prisma.indexRequest.update({
           where: { id: parseInt(id, 10) },
           data: {
@@ -78,6 +117,19 @@ const indexing: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
             status: status ?? undefined,
           },
         });
+
+        //Update URL status
+        if (status === "RUNNING") {
+          await fastify.prisma.indexLink.updateMany({
+            where: {
+              requestId: parseInt(id, 10),
+              status: "PENDING",
+            },
+            data: {
+              status: "INDEXING",
+            },
+          });
+        }
         return reply.status(200).send(updatedIndexRequest);
       } catch (error) {
         handleError(error, request, reply);
@@ -100,6 +152,13 @@ const indexing: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
           userId: userId,
           ...(status ? { status } : {}),
         },
+        select: {
+          id: true,
+          description: true,
+          status: true,
+          createdAt: true,
+          updatedAt: true,
+        },
         orderBy: {
           createdAt: "desc",
         },
@@ -111,10 +170,129 @@ const indexing: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     }
   });
 
-  //POST /api/indexing/url
-  
+  //POST /api/indexing/:requestId
+  interface IndexLinkBody {
+    urls: string[];
+  }
 
+  fastify.post<{
+    Params: { requestId: string };
+    Body: IndexLinkBody;
+  }>("/:requestId", async function (request, reply) {
+    const { requestId } = request.params;
+    const { urls } = request.body;
+    const userId = request.profile?.userId;
+    if (!userId) {
+      return reply.status(401).send({ error: "Unauthorized" });
+    }
 
+    try {
+      const indexRequest = await fastify.prisma.indexRequest.findUnique({
+        where: { id: parseInt(requestId, 10) },
+        select: { status: true, userId: true },
+      });
+
+      if (!indexRequest) {
+        return reply.status(404).send({ error: "Request not found." });
+      }
+      if (indexRequest.userId !== userId) {
+        return reply.status(403).send({
+          error: "You do not have permission to send URLs for this request.",
+        });
+      }
+      if (indexRequest.status !== "DRAFT") {
+        return reply.status(400).send({
+          error: "Cannot send URLs for requests that are not in DRAFT status.",
+        });
+      }
+      const urlArray = typeof urls === "string" ? [urls] : urls;
+      if (urlArray.length > 100) {
+        return reply
+          .status(400)
+          .send({ error: "Cannot send more than 100 URLs at once." });
+      }
+
+      //Validate URL List
+      const invalidUrls: string[] = [];
+      for (const url of urls) {
+        if (!validator.isURL(url, { require_protocol: true })) {
+          invalidUrls.push(url);
+        }
+      }
+      if (invalidUrls.length > 0) {
+        return reply.status(400).send({
+          error: "Invalid URL(s): " + invalidUrls.join(", "),
+        });
+      }
+
+      const indexLinks = await Promise.all(
+        urlArray.map((url) =>
+          fastify.prisma.indexLink.create({
+            data: {
+              requestId: parseInt(requestId, 10),
+              url,
+            },
+            select: {
+              id: true,
+              url: true,
+              status: true,
+              createdAt: true,
+            },
+          })
+        )
+      );
+
+      return reply.status(201).send(indexLinks);
+    } catch (error) {
+      handleError(error, request, reply);
+    }
+  });
+
+  // GET /api/indexing/:requestId
+  fastify.get<{ Params: { requestId: string } }>(
+    "/:requestId",
+    async (request, reply) => {
+      const { requestId } = request.params;
+      const userId = request.profile?.userId;
+
+      if (!userId) {
+        return reply.status(401).send({ error: "Unauthorized" });
+      }
+
+      try {
+        const userRequest = await fastify.prisma.indexRequest.findUnique({
+          where: { id: parseInt(requestId, 10) },
+          select: { userId: true },
+        });
+        if (!userRequest || userRequest.userId !== userId) {
+          return reply
+            .status(403)
+            .send({ error: "Access denied to this requestId" });
+        }
+
+        const indexLinks = await fastify.prisma.indexLink.findMany({
+          where: { requestId: parseInt(requestId, 10) },
+          select: {
+            id: true,
+            url: true,
+            status: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        });
+
+        if (indexLinks.length === 0) {
+          return reply
+            .status(404)
+            .send({ error: "No URLs found for this requestId" });
+        }
+
+        return reply.status(200).send(indexLinks);
+      } catch (error) {
+        handleError(error, request, reply);
+      }
+    }
+  );
 };
 
 export default indexing;
